e34a52c4a433e67cc1d2a21593e25932140e7036
Brian Short <brian.ralph.short@gmail.com>
2021-01-16 22:32:45 -0500

    Add api routes file api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,69 @@
+//Express router
+const router = require("express").Router();
+
+//Using the workout model for database get and put
+const Workout = require("../models/workout.js");
+
+//Post a new exercise array/entry to the database
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Update an individual entry with additional information in the database; 
+//for stacking individual exercises into a longer "routine" object
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Get all workouts for display
+router.get("/api/workouts/all", (req, res) => {
+  Workout.find({})
+    .then(dbWorkouts => {
+      //console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//To get a range of workouts
+router.get("/api/workouts/range", ({ query }, res) => {
+  Workout.find({ day: { $gte: query.start, $lte: query.end } })
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//To delete an individual workout from the database
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Export the router to the server
+module.exports = router;
